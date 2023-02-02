47e3eccb108e682bf5e0f141a0d13cdb1526fa27
5pr4ynpr4y <85466127+5pr4ynpr4y@users.noreply.github.com>
2023-02-02 17:07:31 -0500

    Update chatAI QK.py

--- chatAI QK.py
+++ chatAI QK.py
@@ -25,4 +25,17 @@ prompt = "Write a function that takes in a list of numbers and returns the sum o
 code = generate_code(prompt)
 print(code)
 
+resultWorkerErr := make(chan error)
+defer close(resultWorkerErr)
+go func() {
+	defer cancel()
+	resultWorkerErr <- b.resultWorker(ctx)
+}()
+
+err := b.worker(ctx)
+cancel()
+if err == nil {
+	return <-resultWorkerErr
+}
+return multierror.Append(err, <-resultWorkerErr)
 
