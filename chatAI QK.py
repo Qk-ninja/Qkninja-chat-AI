4ab3bb86b073c31c5caad6bfba224c3bd75dea5f
5pr4ynpr4y <85466127+5pr4ynpr4y@users.noreply.github.com>
2023-02-02 17:59:45 -0500

    Update chatAI QK.py
    This script uses the Flask library to create a simple web server that listens for POST requests on the /gpt3 endpoint. When it receives a request, it extracts the prompt from the request body, generates text using the OpenAI GPT-3 model, and returns the generated text as a JSON response.
    
    You can run this script locally on your computer by opening a terminal window, navigating to the folder where the script is located, and running the command python app.py. This will start the web server and it will be listening for requests on http://localhost:8080/gpt3
    
    To run it on visual studio you have to install flask and openai python packages, and also make sure that you have python 3 installed on your computer.
    
    Make sure to replace "YOUR_API_KEY" with your own API key that you can get by creating an account in https://beta.openai.com/signup/.
    
    Also, this is just a basic example and you may need to modify the code to suit your specific requirements and use case.

--- chatAI QK.py
+++ chatAI QK.py
@@ -1,23 +1,31 @@
 import openai
+from flask import Flask, jsonify, request
 
-# Use the API key from your OpenAI account
+# Set your API key
 openai.api_key = "YOUR_API_KEY"
 
-# Define the prompt, or the text that you want GPT-3 to complete
-prompt = "Write a chatbot for python 3"
+# Initialize the Flask app
+app = Flask(__name__)
 
-# Use the completions() function to generate text
-completions = openai.Completion.create(
-    engine="text-davinci-002",
-    prompt=prompt,
-    max_tokens=1024,
-    n=1,
-    stop=None,
-    temperature=0.5,
-)
+# Define a route for the endpoint
+@app.route('/gpt3', methods=['POST'])
+def generate_text():
+    # Get the prompt from the request
+    prompt = request.json['prompt']
+    
+    # Generate text
+    completions = openai.Completion.create(engine="text-davinci-002",
+                                      prompt=prompt,
+                                      max_tokens=1024,
+                                      n=1,
+                                      stop=None,
+                                      temperature=0.5)
 
-# The generated text is stored in the message property
-message = completions.choices[0].text
+    # Get the generated text
+    text = completions.choices[0].text
+    
+    # Return the generated text as a JSON response
+    return jsonify({'text': text})
 
-# Print the generated text
-print(message)
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=8080)
